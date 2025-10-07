1bc07586e2f1b10e0b53ff0151d6959f864bc990
MatheKornel <mathekornel@gmail.com>
2025-10-07 20:13:58 +0200

    beégetett guitar_notes lista valid hangok kiszűréséhez, bináris keresés

--- backend/note_recognition.py
+++ backend/note_recognition.py
@@ -8,6 +8,13 @@ class ShortTimeFT:
     def note_rec(self, max_harmonics):
         nfft = 4096
         fs = 44100
+        guitar_notes = [
+            82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81,
+            138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65,
+            220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63,
+            349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25,
+            554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61,
+            880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51]
 
         #STFT
         D = lb.stft(self.filtered, n_fft=nfft, hop_length=512, window="blackman", center=False)
@@ -77,7 +84,22 @@ class ShortTimeFT:
                     notes.append(avg_note)
             return notes
                 
-        
+        #Bináris keresés a guitar_notes-ban
+        def binary_serach(notes, value):
+            left = 0
+            right = len(notes) - 1
+            while True:
+                center = (left + right) / 2
+                if notes[center] > value:
+                    right = center - 1
+                elif notes[center] < value:
+                    left = center + 1
+                if not (left <= right and notes[center] >= abs(notes[center] - value)):
+                    break
+            exist = left <= right
+            return exist
+                
+
         all_notes = select_notes(all_f0, 2, 30)
 
         formatted_notes = [f"{float(f):.2f} Hz" for f in all_notes]
