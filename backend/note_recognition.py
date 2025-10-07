a9d1e2ca6c327969002be6d479db95e5a1879c87
MatheKornel <mathekornel@gmail.com>
2025-10-07 18:11:21 +0200

    Ablakméret növelése 4096-ra, Blackman ablakozás használata, frekvencia kerekítése csak a végén, szebb kiiratás

--- backend/note_recognition.py
+++ backend/note_recognition.py
@@ -1,17 +1,16 @@
 import librosa as lb
 import numpy as np
-import statistics
 
 class ShortTimeFT:
     def __init__(self, filtered):
         self.filtered = filtered
 
     def note_rec(self, max_harmonics):
-        nfft = 2048
+        nfft = 4096
         fs = 44100
 
         #STFT
-        D = lb.stft(self.filtered, n_fft=nfft, hop_length=512, window="hamming", center=False)
+        D = lb.stft(self.filtered, n_fft=nfft, hop_length=512, window="blackman", center=False)
         freqs = lb.fft_frequencies(sr=44100, n_fft=nfft)
         magnitude = np.abs(D)
         dominant_bins = magnitude.argmax(axis=0)
@@ -35,7 +34,7 @@ class ShortTimeFT:
                 salience = amp*s
                 s = s/4
                 salience = 1-np.exp(-salience)
-                harmonics.append((round(freq, 4), round(amp, 4), round(salience, 4)))
+                harmonics.append((freq, amp, salience))
                 #print(harmonics)
             max_salience = max(harmonics, key=lambda x: x[2])
             f0_candidate = max_salience[0]
@@ -56,7 +55,7 @@ class ShortTimeFT:
                 else:
                     break
             #print(f"{best_f0}")
-            all_f0.append(round(best_f0, 2))
+            all_f0.append(best_f0)
 
         def select_notes(f0_list, tol, min_frames):
             notes = []
@@ -68,12 +67,12 @@ class ShortTimeFT:
                     current_note.append(f)
                 else:
                     if len(current_note) >= min_frames:
-                        avg_note=round(np.mean(current_note), 2)
+                        avg_note=np.mean(current_note)
                         if not notes or abs(avg_note - notes[-1]) > tol:
                             notes.append(avg_note)
                     current_note = [f]
             if current_note and len(current_note) >= min_frames:
-                avg_note=round(np.mean(current_note), 2)
+                avg_note=np.mean(current_note)
                 if not notes or abs(avg_note - notes[-1]) > tol:
                     notes.append(avg_note)
             return notes
@@ -81,7 +80,8 @@ class ShortTimeFT:
         
         all_notes = select_notes(all_f0, 2, 30)
 
-        print(f"All f0 frequencies: {all_notes}")
+        formatted_notes = [f"{float(f):.2f} Hz" for f in all_notes]
+        print("All f0 frequencies:", ", ".join(formatted_notes))
 
 
 
